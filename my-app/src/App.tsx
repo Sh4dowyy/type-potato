e862d1398ccf9475bd4720037a5119af055e1971
Sh4dowyy <dmitrii.agarjov@gmail.com>
2024-09-16 16:52:23 +0300

    a

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import MyContainer from "./layout/MyContainer";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <MyContainer>
+      <Login />
+    </MyContainer>
+  );
+}
+
+export default App;
