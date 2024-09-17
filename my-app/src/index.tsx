b60dbfcfe81ef08f2c9ddbaf0297673c6ed68953
Sh4dowyy <dmitrii.agarjov@gmail.com>
2024-09-17 15:12:56 +0300

    feat: ReactDOM routes added

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import MyForm from "./components/Login";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/form" element={<MyForm />} />
+      <Route path="/extra" element={<h1>Extra</h1>} />
+      <Route path="/contact" element={<h1>Contact</h1>} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
