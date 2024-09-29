84645beb4f0a5df5eddc19c785b417846cdb13da
Sh4dowyy <dmitrii.agarjov@gmail.com>
2024-09-29 16:07:19 +0300

    feat: prettier added, Header added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,16 @@
-import "./App.css";
-import MyContainer from "./layout/MyContainer";
-import Login from "./components/Login";
-import { Outlet } from "react-router-dom";
+import "./App.css"
+import MyContainer from "./layout/MyContainer"
+import Login from "./components/Login"
+import { Outlet } from "react-router-dom"
+import { Typography } from "@mui/material"
 
 function App() {
   return (
     <>
-      <h1>Header</h1>
+      <Typography variant = "h3" >Header</Typography>
       <Outlet />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
